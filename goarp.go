432c26855534fd0b317dd88c303929fbee31bc64
Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>
2016-05-29 11:05:50 +0200

    Ignore never active host
    Signed-off-by: Jean-Laurent de Morlhon <jeanlaurent@morlhon.net>

--- goarp.go
+++ goarp.go
@@ -1,5 +1,22 @@
 package main
 
+// Find IP Range
+// ifconfig en0 | grep -v inet6 | grep inet | cut -d ' ' -f 2
+// 10.0.1.25
+
+// Discard name with ?
+
+// find more info on a host
+//nmap -sn -oG - -e en0 10.0.1.1/24
+//# Nmap 7.01 scan initiated Sat May 28 19:00:23 2016 as: nmap -sn -oG - -e en0 10.0.1.1/24
+//Host: 10.0.1.1 (livebox.home)	Status: Up
+//Host: 10.0.1.2 (timecapsule.home)	Status: Up
+//Host: 10.0.1.18 (ll.home)	Status: Up
+//Host: 10.0.1.20 (t.home)	Status: Up
+//Host: 10.0.1.25 (ll.home)	Status: Up
+//Host: 10.0.1.69 (ipad-morlhon.home)	Status: Up
+//# Nmap done at Sat May 28 19:00:31 2016 -- 256 IP addresses (6 hosts up) scanned in 8.24 seconds
+
 import (
 	"fmt"
 	"os/exec"
@@ -16,6 +33,9 @@ type HostList struct {
 func (h *HostList) Add(host Host) bool {
 	h.lock.Lock()
 	defer h.lock.Unlock()
+	if host.Name == "?" {
+		return false
+	}
 	found := false
 	for _, targetHost := range h.Hosts {
 		if targetHost.Name == host.Name && targetHost.IP == host.IP {
@@ -34,6 +54,10 @@ func (h *HostList) Print() {
 	for _, host := range h.Hosts {
 		fmt.Println(host)
 	}
+}
+
+func (h *HostList) Size() int {
+	return len(h.Hosts)
 }
 
 func NewHostList() *HostList {
@@ -81,16 +105,14 @@ func runArp(onHost func(Host)) error {
 }
 
 func run(list *HostList) {
-	var count int
 	err := runArp(func(host Host) {
-		count++
 		if list.Add(host) {
-			fmt.Println("Adding new host ", host)
+			fmt.Println("Adding new host", host)
 		}
 	})
-	fmt.Println("Host found on this run", count)
+	fmt.Println("Host detect on this run", list.Size())
 	if err != nil {
-		fmt.Println("Error on arp subsystem: ", err)
+		fmt.Println("Error on arp subsystem:", err)
 	}
 }
 
@@ -113,7 +135,7 @@ func main() {
 			}
 		}
 	}()
-	for runNumber < 4 {
+	for runNumber < 20 { // 4 is 3
 		time.Sleep(1 * time.Second)
 	}
 	close(quit)
